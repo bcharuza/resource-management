78a24176b9f594a0393a51a2d96cd6fc3b61eed4
bcharuza <bcharuza@yahoo.com>
2021-05-27 01:15:15 +0200

    [fix] Protect against overflow in Logger.cpp

--- examples/problem/Logger.cpp
+++ examples/problem/Logger.cpp
@@ -1,23 +1,23 @@
 #include "Logger.hpp"
 #include "CfgReader.hpp"
-#include <atomic>
 #include <cstdarg>
-#include <cstdio>
-#include <thread>
-#include <sstream>
-char const* s_category[] =
-  {"FATAL","ERROR","WARNING","INFO"};
 
-void trace(TraceLevel lvl,char const* fmt, ...){
-  char buf[100];
+static char const*
+cat(TraceLevel arg) noexcept{
+  static char const* l[] =
+    {"FATAL","ERROR","WARNING","INFO","<NULL>"};
+  static const size_t s = sizeof(l)/sizeof(char*);
+  return l[std::min((size_t)arg, s)];
+}
+void trace(TraceLevel lvl,char const* fmt,...){
+  char buf[64];
   va_list argptr;
   va_start(argptr,fmt);
-  
   if(CfgReader::s_logTime){
     time_t t = time(nullptr);
-    sprintf(buf,"%s(%s[t:%i])[%s] %s\n",ctime(&t),CfgReader::s_sysName.c_str(),CfgReader::threadId(),s_category[lvl],fmt);
+    sprintf(buf,"%s(%s[t:%i])[%s] %s\n",ctime(&t),CfgReader::s_sysName.c_str(),CfgReader::threadId(),cat(lvl),fmt);
   }else
-    sprintf(buf,"(%s[t:%i])[%s] %s\n",CfgReader::s_sysName.c_str(),CfgReader::threadId(),s_category[lvl],fmt);
+    sprintf(buf,"(%s[t:%i])[%s] %s\n",CfgReader::s_sysName.c_str(),CfgReader::threadId(),cat(lvl),fmt);
   vfprintf(stderr,buf,argptr);
   va_end(argptr);
 }
--- examples/resource/Logger.cpp
+++ examples/resource/Logger.cpp
@@ -1,28 +1,29 @@
 #include "Logger.hpp"
 #include "CfgReader.hpp"
-#include <atomic>
 #include <cstdarg>
-#include <cstdio>
-#include <thread>
-#include <sstream>
-char const* s_category[] =
-  {"FATAL","ERROR","WARNING","INFO"};
 
-void trace(TraceLevel lvl,char const* fmt, ...){
-  char buf[100];
+static char const*
+cat(TraceLevel arg) noexcept{
+  static char const* l[] =
+    {"FATAL","ERROR","WARNING","INFO","<NULL>"};
+  static const size_t s = sizeof(l)/sizeof(char*);
+  return l[std::min((size_t)arg, s)];
+}
+void trace(TraceLevel lvl,char const* fmt,...){
+  char buf[64];
   va_list argptr;
   va_start(argptr,fmt);
-  std::string sysName;
   bool logTime;
+  std::string sysName;
   std::tie(logTime,sysName) =
     getGlobalCfg().critical_section([](auto const& x){;
 	return std::make_pair(x.m_logTime,x.m_sysName);
 				    });
   if(logTime){
     time_t t = time(nullptr);
-    sprintf(buf,"%s(%s[t:%i])[%s] %s\n",ctime(&t),sysName.c_str(),getThreadId(),s_category[lvl],fmt);
+    sprintf(buf,"%s(%s[t:%i])[%s] %s\n",ctime(&t),sysName.c_str(),getThreadId(),cat(lvl),fmt);
   }else
-    sprintf(buf,"(%s[t:%i])[%s] %s\n",sysName.c_str(),getThreadId(),s_category[lvl],fmt);
+    sprintf(buf,"(%s[t:%i])[%s] %s\n",sysName.c_str(),getThreadId(),cat(lvl),fmt);
   vfprintf(stderr,buf,argptr);
   va_end(argptr);
 }
